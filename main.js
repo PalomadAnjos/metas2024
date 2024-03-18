8e7e299969b9f59f526266d63068c4709b86d24e
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2024-03-18 10:38:38 -0300

    ok

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ for(let=0;i <botoes.length;++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo = new Date("2024-07-04T00:00:00);
+let tempoAtual = new Date ();
 contadores[0].textContent = tempoObjetivo1;
 
 
