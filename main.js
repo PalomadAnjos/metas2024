712aacc6018fbad7731dac7403c55bc5ff1cab10
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2024-04-05 11:17:57 -0300

    ok

--- main.js
+++ main.js
@@ -15,4 +15,19 @@ const tempoObjetivo = new Date("2024-07-04T00:00:00);
 let tempoAtual = new Date ();
 contadores[0].textContent = tempoObjetivo1;
 
+function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date () ;
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = math.floor(tempoFinal/1000);
+    let minutos = math.floor(segunos/60);
+    let horas = math.floor(minutos/60);
+    let dias = math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
+
 
